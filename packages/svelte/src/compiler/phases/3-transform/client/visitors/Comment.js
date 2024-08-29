63a946ac2ccc69ed31e9d6d17b81a3fc426b547d
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-08-29 10:29:37 -0700

    perf: inline module variables into template

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/Comment.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/Comment.js
@@ -1,11 +1,13 @@
 /** @import { Comment } from '#compiler' */
 /** @import { ComponentContext } from '../types' */
 
+import { push_template_quasi } from '../utils.js';
+
 /**
  * @param {Comment} node
  * @param {ComponentContext} context
  */
 export function Comment(node, context) {
 	// We'll only get here if comments are not filtered out, which they are unless preserveComments is true
-	context.state.template.push(`<!--${node.data}-->`);
+	push_template_quasi(context.state, `<!--${node.data}-->`);
 }
